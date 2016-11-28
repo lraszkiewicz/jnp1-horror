c08e634dab5106aec5fde40a9b346d3e8de27d91
adasiek96 <adasiek.96@outlook.com>
2016-11-28 21:15:37 +0100

    Major changes

--- citizen.h
+++ citizen.h
@@ -6,59 +6,43 @@
 
 using namespace std;
 
-enum citizen {
-    adult, teenager, sheriff
+enum class citizen {
+    ADULT, TEENAGER, SHERIFF
 };
 
-template <typename T, citizen>
-class Citizen;
-
-template <typename T>
-using Adult = Citizen<T, adult>;
-template <typename T>
-using Teenager = Citizen<T, teenager>;
-template <typename T>
-using Sheriff = Citizen<T, sheriff>;
-
-template <typename T>
-class Citizen<T, adult> {
+template <typename T, citizen C>
+class Citizen {
+    static_assert(is_arithmetic<T>::value);
 public:
-    Citizen<T, adult>(T health, T age) : _health(health), _age(age) {
-        assert((T)18 <= age && age <= (T)100);
+    template<typename U = T, typename =
+        enable_if_t<C == citizen::TEENAGER || C == citizen::ADULT, U>>
+    Citizen(T health, T age) : _health(health), _age(age) {
+        if (C == citizen::TEENAGER)
+            assert(static_cast<T>(11) <= age && age <= static_cast<T>(17));
+        else
+            assert(static_cast<T>(18) <= age && age <= static_cast<T>(100));
     }
-    T getHealth() { return _health; }
-    T getAge() { return _age; }
-    void takeDamage(T damage) { _health = max(_health - damage, (T)0); }
-private:
-    T _health, _age;
-};
-
-template <typename T>
-class Citizen<T, teenager> {
-public:
-    Citizen<T, teenager>(T health, T age) : _health(health), _age(age) {
-        assert((T)11 <= age && age <= (T)17);
-    }
-    T getHealth() { return _health; }
-    T getAge() { return _age; }
-    void takeDamage(T damage) { _health = max(_health - damage, (T)0); }
-private:
-    T _health, _age;
-};
-
-template <typename T>
-class Citizen<T, sheriff> {
-public:
-    Citizen<T, sheriff>(T health, T age, T attack_power)
+    template<typename U = T, typename = enable_if_t<C == citizen::SHERIFF, U>>
+    Citizen(T health, T age, T attack_power)
             : _health(health), _age(age), _attack_power(attack_power) {
-        assert((T)18 <= age && age <= (T)100);
+        assert(static_cast<T>(18) <= age && age <= static_cast<T>(100));
     }
     T getHealth() { return _health; }
     T getAge() { return _age; }
+    template<typename U = T, typename = enable_if_t<C == citizen::SHERIFF, U>>
     T getAttackPower() { return _attack_power; }
-    void takeDamage(T damage) { _health = max(_health - damage, (T)0); }
+    void takeDamage(T damage) {
+        _health = max(_health - damage, static_cast<T>(0));
+    }
 private:
     T _health, _age, _attack_power;
 };
+
+template <typename T>
+using Adult = Citizen<T, citizen::ADULT>;
+template <typename T>
+using Teenager = Citizen<T, citizen::TEENAGER>;
+template <typename T>
+using Sheriff = Citizen<T, citizen::SHERIFF>;
 
 #endif
