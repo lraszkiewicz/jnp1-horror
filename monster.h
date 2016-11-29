354a06bd38bf7b950bb196fcb3a43371c6fcbe8a
adasiek96 <adasiek.96@outlook.com>
2016-11-29 13:34:28 +0100

    The first release

---
+++ monster.h
@@ -0,0 +1,47 @@
+#ifndef MONSTER_H
+#define MONSTER_H
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+enum class monster {
+    ZOMBIE, VAMPIRE, MUMMY
+};
+
+template <typename T, monster>
+class Monster {
+public:
+    typedef T valueType;
+    template<typename U = T, typename = enable_if_t<is_arithmetic<T>::value, U>>
+    Monster(T health, T attack_power)
+            : _health(health), _attack_power(attack_power) {}
+    T getHealth() { return _health; }
+    T getAttackPower() { return _attack_power; }
+    void takeDamage(T damage) {
+        _health = max(_health - damage, static_cast<T>(0));
+    }
+private:
+    T _health, _attack_power;
+};
+
+template <typename T>
+using Zombie = Monster<T, monster::ZOMBIE>;
+template <typename T>
+using Vampire = Monster<T, monster::VAMPIRE>;
+template <typename T>
+using Mummy = Monster<T, monster::MUMMY>;
+
+template <typename T1, monster m, typename T2, citizen c>
+void attack(Monster<T1, m>& monster, Citizen<T2, c>& victim) {
+    victim.takeDamage(monster.getAttackPower());
+}
+
+template <typename T1, monster m, typename T2>
+void attack(Monster<T1, m>& monster, Citizen<T2, citizen::SHERIFF>& victim) {
+    victim.takeDamage(monster.getAttackPower());
+    monster.takeDamage(victim.getAttackPower());
+}
+
+#endif
